5dce2351651c16b01aafc8705b4bc50ccaf31239
bimbo-ade <bimboadeyemi65@gmail.com>
2022-11-12 10:35:38 +0100

    style: add custome style to tailwind config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl text-red font-bold underline">Hello world!</h1>
+      <h2 className="text-green text-3xl">hey hey</h2>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      red: " #A02279",
+      black: "#434343",
+      white: "#FFFFFF",
+      bgBlack: " #1D1D1E;",
+    },
     extend: {},
   },
   plugins: [],
